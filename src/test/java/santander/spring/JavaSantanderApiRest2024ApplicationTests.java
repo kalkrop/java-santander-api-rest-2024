daa7d236158580137e71b280ef776a029d806250
Anderson de Araújo Alves <anderson.araujoa@gmail.com>
2024-05-28 05:19:42 -0400

    first commit

---
+++ src/test/java/santander/spring/JavaSantanderApiRest2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package santander.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSantanderApiRest2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
