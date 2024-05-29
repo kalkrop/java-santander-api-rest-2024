57c5d8c0b26256c6286d69f2a2272946bd4dfa3c
Anderson de Araújo Alves <anderson.araujoa@gmail.com>
2024-05-29 07:03:11 -0400

    feat: Handle Business Exceptions

---
+++ src/main/java/santander/spring/service/exception/NotFoundException.java
@@ -0,0 +1,11 @@
+package santander.spring.service.exception;
+
+public class NotFoundException extends BusinessException {
+
+    private static final long serialVersionUID = 1L;
+
+    public NotFoundException() {
+        super("Resource not found.");
+    }
+    
+}
